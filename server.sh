307327c9f714cdb76528c5cec699dbad7da9519b
jeruen <jeruen@gmail.com>
2018-04-04 10:28:36 +0800

    server.sh

---
+++ server.sh
@@ -0,0 +1,13 @@
+#! /bin/sh  
+  
+while true ; do  
+     NUM=`ps aux | grep -w tao-vpn-lite-1.0.jar | grep -v grep |wc -l`  
+     #echo $NUM  
+      if [ "${NUM}" -lt "1" ];then  
+         echo "tao-vpn-lite-1.0.jar was killed"  
+         nohup java -cp ~/tao-vpn-lite-1.0.jar org.tao.vpn.lite.Server 8082 >/dev/null 2>&1 &
+     fi
+     sleep 5s  
+ done  
+   
+ exit 0 
